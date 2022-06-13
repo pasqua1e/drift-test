bdf0b7eb8ab9a8320a5474e5815c5e97dd7ab1c4
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2022-06-13 20:09:37 +0100

    tt

--- s3.tf
+++ s3.tf
@@ -1,17 +1,3 @@
 resource "aws_s3_bucket" "drift-test2" {
   bucket = "drift-test2"
-<<<<<<< HEAD
-=======
-
-  tags = {
-    git_commit           = "b83cd72d98475369c7162ecd1f0b31ac3897fec0"
-    git_file             = "s3.tf"
-    git_last_modified_at = "2022-06-13 19:01:45"
-    git_last_modified_by = "31853954+pasqua1e@users.noreply.github.com"
-    git_modifiers        = "31853954+pasqua1e"
-    git_org              = "pasqua1e"
-    git_repo             = "drift-test"
-    yor_trace            = "0f28ddd1-3ef3-487f-945a-b0cb0fe74195"
-  }
->>>>>>> 868aba1111fb418e74c5bb91664ed2c0caa0b575
 }
